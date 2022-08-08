b0a0db3dd59553ab4a39beb1af9a0782574306d4
Weijun Yu <weijun.yu@sap.com>
2022-08-08 22:53:15 +0800

    spring-beans-3.2.18 source code and tests

---
+++ spring-beans-3.2.18/src/test/java/org/springframework/beans/factory/xml/DummyBoImpl.java
@@ -0,0 +1,20 @@
+package org.springframework.beans.factory.xml;
+
+/**
+ * 
+ * @author Rod Johnson
+ * @version $Id: DummyBoImpl.java,v 1.2 2004-03-18 03:01:16 trisberg Exp $
+ */
+public class DummyBoImpl implements DummyBo {
+	
+	DummyDao dao;
+
+	public DummyBoImpl(DummyDao dao) {
+		this.dao = dao;
+	}
+	
+	public void something() {
+		
+	}
+
+}
